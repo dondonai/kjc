686f71857626f7b63ecf0851a6a5037768f1c3e5
dOndOnAi <adones.kjc@gmail.com>
2014-08-02 20:47:44 +0800

    design manna page

--- category-manna.php
+++ category-manna.php
@@ -6,18 +6,22 @@ function kjc_class( $classes ) {
 	return $classes;
 }
 
+add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_full_width_content' );
+
 // remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_open', 5 );
 // remove_action( 'genesis_entry_header', 'genesis_post_info', 12 );
 // remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_close', 15 );
 
-// remove_action( 'genesis_entry_footer', 'genesis_entry_footer_markup_open', 5 );
-// remove_action( 'genesis_entry_footer', 'genesis_post_meta' );
-// remove_action( 'genesis_entry_footer', 'genesis_entry_footer_markup_close', 15 );
+remove_action( 'genesis_entry_footer', 'genesis_entry_footer_markup_open', 5 );
+remove_action( 'genesis_entry_footer', 'genesis_post_meta' );
+remove_action( 'genesis_entry_footer', 'genesis_entry_footer_markup_close', 15 );
 
 add_action( 'genesis_loop', 'kjc_title', 1 );
 
 function kjc_title() {
-	echo '<h2 class="entry-title">Manna</h2>';
+	echo '<article class="manna-info entry"><h2 class="entry-title">Manna of Revelations</h2>
+	<p>As preached by Pastor Apollo C. Quiboloy on his daily programs on Sonshine Media Network International (SMNI).</p>
+	</article>';
 }
 
 genesis();
